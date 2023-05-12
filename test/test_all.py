7afe5afd1343bc616f17838da078f7570c3689b7
Tim Blazytko <tim@blazytko.to>
2023-05-12 13:20:45 +0200

    Squashed 'miasm/' changes from d82c9ef5..2b8366aa
    2b8366aa Merge pull request #1449 from cea-sec/fix-imprec
    58f81a51 ImpRec: recover_import without alignment hypothesis
    2475c946 ImpRec: fix the search to actually use the ptrsize
    49ec4bc9 Merge pull request #1448 from cea-sec/generic-unpack
    230d528c Merge pull request #1447 from cea-sec/helper-depgraph
    20aff863 Merge pull request #1446 from cea-sec/example-symbexec
    b4569e45 Merge pull request #1445 from cea-sec/doc-cheatsheet
    b66becde Add a sandbox example using the ImpRec strategy
    0aed1354 Add a "generic" import recovery strategy based on ImpRec
    d32bf638 Depgraph: add `.address_to_location` to help find the line and loc of an address
    17552f09 Example: add support for symbol name in `address`
    41f3c6ac Example: add a basic symbol_exec example
    9e39991a Doc: add Sandbox + Disassembler/Lifter cheatsheet
    2a392ad2 doc/README: summarize documentations' location
    00d4fd4b Merge pull request #1444 from serpilliere/version_0.1.5
    23873fcd Release v0.1.5
    caa300a4 Merge pull request #1443 from serpilliere/fix_setup_header
    a0fc6cfe Fix setup header
    f3a0f26a Merge pull request #1440 from serpilliere/version_0.1.4
    4288ef2e Release v0.1.4
    5a09e18f Merge pull request #1441 from serpilliere/use_pkg_llvmlite
    75641cbf Use llvmlite package
    
    git-subtree-dir: miasm
    git-subtree-split: 2b8366aaa88679609ae56e738ebf9e2f47aa53fc

--- test/test_all.py
+++ test/test_all.py
@@ -739,6 +739,7 @@ class ExampleSymbolExec(Example):
 
 
 testset += ExampleSymbolExec(["single_instr.py"])
+testset += ExampleSymbolExec(["symbol_exec.py", "--steps", Example.get_sample("box_upx.exe")])
 for options, nb_sol, tag in [([], 8, []),
                              (["-i", "--rename-args"], 12, [TAGS["z3"]])]:
     testset += ExampleSymbolExec(["depgraph.py",
@@ -797,6 +798,12 @@ for jitter in ExampleJitter.jitter_engines:
                               Example.get_sample("box_upx.exe")] +
                              ["--jitter", jitter],
                              products=[Example.get_sample("box_upx_exe_unupx.bin")],
+                             tags=tags.get(jitter, []))
+
+    testset += ExampleJitter(["unpack_generic.py",
+                              Example.get_sample("box_upx.exe")] +
+                             ["--jitter", jitter, "-o"],
+                             products=[Example.get_sample("box_upx.exe.dump")],
                              tags=tags.get(jitter, []))
 
     testset += ExampleJitter(["memory_breakpoint.py",
